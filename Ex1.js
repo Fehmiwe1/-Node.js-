2991a699af11e7f788452842c947f4c4a5c6e28d
Fehmiwe1 <fehmiwe1@gmail.com>
2024-11-27 20:43:54 +0200

    Add files via upload
    פהמי והבי + מאור דוד

---
+++ Ex1.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const number = 123;
+
+let two = number % 2 == 0;
+let three = number % 3 == 0;
+let five = number % 5 == 0;
+
+console.log(Number(two) + Number(three) + Number(five));
