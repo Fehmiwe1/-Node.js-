27e6e2779bb12483c75b0dc26e078e69792f15f7
Fehmiwe1 <fehmiwe1@gmail.com>
2024-11-27 21:15:54 +0200

    Update Ex1.js

--- Ex1.js
+++ Ex1.js
@@ -4,8 +4,13 @@
 
 const number = 123;
 
+// Check if the number is divisible by 2
 let two = number % 2 == 0;
+// Check if the number is divisible by 3
 let three = number % 3 == 0;
+// Check if the number is divisible by 5
 let five = number % 5 == 0;
 
+// Convert the boolean values to 1 or 0 using Number(), and sum them
+// If the number is divisible by 2, 3, or 5, it adds 1 to the sum; otherwise, it adds 0
 console.log(Number(two) + Number(three) + Number(five));
