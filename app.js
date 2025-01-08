6219e847b6ae5f5b78bbb6373c94ec77164d32d7
Fehmiwe1 <fehmiwe1@gmail.com>
2025-01-08 13:53:33 +0200

    Ex8

--- app.js
+++ app.js
@@ -1,73 +1,31 @@
-const fs = require("fs");
+// פהמי והבי + מאור דוד
 const express = require("express");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-const users = JSON.parse(fs.readFileSync(__dirname + "/users.json"));
+const { products } = require("./data");
 
-// Get all users
-app.get("/", (req, res) => {
-  res.status(200).json(users);
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, price } = product;
+    return { id, price };
+  });
+  res.json(newProducts);
 });
 
-// Get all users
-app.get("/api/users", (req, res) => {
-  res.status(200).json(users);
-});
+app.get("/api/products/:productPrice", (req, res) => {
+  const { productPrice } = req.params;
 
-// Get users by age range
-app.get("/api/users/filter", (req, res) => {
-  const { minAge, maxAge } = req.query;
-
-  if (!minAge || !maxAge) {
-    return res.status(400).send("minAge and maxAge must be provided.");
-  }
-
-  const min = Number(minAge);
-  const max = Number(maxAge);
-
-  const filteredUsers = users.filter(
-    (user) => user.age >= min && user.age <= max
+  const singleProduct = products.filter(
+    (product) => product.price > Number(productPrice)
   );
 
-  if (filteredUsers.length === 0) {
-    return res.status(404).send("No users found in the specified age range.");
-  }
-
-  res.status(200).json(filteredUsers);
-});
-
-// Get user by ID
-app.get("/api/users/:id", (req, res) => {
-  const { id } = req.params;
-  const singleUser = users.find((user) => user.id === Number(id));
-
-  if (!singleUser) {
+  if (!singleProduct) {
     return res.status(404).send("Product Does Not Exist");
   }
 
-  res.status(200).json(singleUser);
-});
-
-app.get("/api/user/:id", (req, res) => {
-  const { id } = req.params;
-  const singleUser = users.find((user) => user.id === Number(id));
-
-  if (!singleUser) {
-    return res.status(404).send("Product Does Not Exist");
-  }
-
-  res.send(
-    `<html>
-      <head></head>
-      <body>
-        <h1>PersonID: ${singleUser.id}</h1>
-        <h1>PersonName: ${singleUser.name}</h1>
-        <h1>PersonEmail: ${singleUser.email}</h1>
-        <h1>PersonEmail: ${singleUser.ages}</h1>
-      </body>
-    </html>`
-  );
+  return res.json(singleProduct);
 });
 
 app.listen(port, () => {
